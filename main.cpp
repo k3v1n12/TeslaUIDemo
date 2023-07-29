456260977618d98d8588ce550e87db314fb51f66
k3v1n12 <ambalikevin@gmail.com>
2023-07-29 19:28:13 +0100

    topp display

--- main.cpp
+++ main.cpp
@@ -1,16 +1,23 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <controller/system.h>
+#include <QQmlContext>
 
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
+    System m_systemHandler;
+
     QQmlApplicationEngine engine;
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed,
         &app, []() { QCoreApplication::exit(-1); },
         Qt::QueuedConnection);
     engine.loadFromModule("TeslaUIDemo", "Main");
+
+    QQmlContext* context (engine.rootContext());
+    context->setContextProperty("systemHandler", &m_systemHandler);
 
     return app.exec();
 }
